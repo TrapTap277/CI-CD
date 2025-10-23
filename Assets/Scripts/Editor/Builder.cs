8228035dd143d78c8f1a5b33fbae21158ae07398
Olexander Ludanov <sway8318@gmail.com>
2025-10-23 21:30:24 +0300

    F! Add builder

---
+++ Assets/Scripts/Editor/Builder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Editor
+{
+    public class Builder
+    {
+        [MenuItem("Build/📦 Android")]
+        public static void BuildAndroid()
+        {
+            var report = BuildPipeline.BuildPlayer(new BuildPlayerOptions
+            {
+                locationPathName = $"./artifacts/Build{Application.version}.apk",
+                target = BuildTarget.Android,
+                options = BuildOptions.None,
+                scenes = new[] { "Assets/ThirdParty/FPS/Scenes/MainScene.unity" }
+            });
+
+            if(report.summary.result != BuildResult.Succeeded)
+                throw new Exception("Build failed see log for details");
+        }
+    }
+}
